b451898e07d26e640428792d2c394a4c5478c232
Simona Pizio <simona.pizio@gmail.com>
2022-07-15 15:56:47 +0200

    Modificati i file di configurazione di Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,11 @@
+import '../src/index.css';
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+/*parameters sono in genere utilizzati per controllare il comportamento delle 
+funzionalità e dei componenti aggiuntivi di Storybook */
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: "^on[A-Z].*" },  /*actions ci consente di creare callback che 
+          appaiono nel pannello delle azioni dell'interfaccia utente di Storybook quando si fa clic.  */
   controls: {
     matchers: {
       color: /(background|color)$/i,
